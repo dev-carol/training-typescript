39c24f0cc648ffb4393898a29b6a07f15fa81ff1
carol santos <contato.caroline1999@gmail.com>
2022-01-03 18:29:27 -0300

    refactor: implement abstract in class view

--- app/views/view.ts
+++ app/views/view.ts
@@ -1,4 +1,5 @@
-export class View <T> {
+export abstract class View <T> {
+    
     protected elemento: HTMLElement;
 
     constructor(seletor: string){
@@ -9,10 +10,8 @@ export class View <T> {
         const template = this.template(model);
         this.elemento.innerHTML = template;
     }
-
     
-    template(model: T): string{
-       throw Error(`Classe filha precisa implementar o método template, ${model}`);
-    }
+        abstract template(model: T): string;
 
+     
 }
